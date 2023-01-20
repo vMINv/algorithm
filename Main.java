c478e0773251e16a375b4522136e83cae06909b9
ShimHyunmin <114932365+vMINv@users.noreply.github.com>
2023-01-20 05:19:38 +0000

    ver1

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 2;
+    int result = 0;
+    if(a>b){
+      result = a;
+    } else if(a<b){
+      result = b;
+    } else {
+      result = a;
+    }
+    System.out.println(result);
+  }
+}
